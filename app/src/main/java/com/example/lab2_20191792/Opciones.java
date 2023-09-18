13d092a90120410fe3c446b9ae863bb9d053acde
angie2512 <a20191792@pucp.edu.pe>
2023-09-17 19:18:40 -0500

    avancefinal

--- app/src/main/java/com/example/lab2_20191792/Opciones.java
+++ app/src/main/java/com/example/lab2_20191792/Opciones.java
@@ -2,25 +2,54 @@ package com.example.lab2_20191792;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.lab2_20191792.dto.Person;
+import com.squareup.picasso.Picasso;
 
 public class Opciones extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opciones);
-    }
+        Toast.makeText(this, "Current Activity: " + getClass().getSimpleName(), Toast.LENGTH_SHORT).show();
+        Intent intent = getIntent();
+        Person person = (Person) intent.getSerializableExtra("person");
+        assert person != null;
+        Log.d("msg-test", "Name: " + person.getResults().get(0).getName().getFirst());
 
-    public void abrirOtraActivity3(View view){
-        Intent intent = new Intent(this,ContadorActivity.class);
-        startActivity(intent);
-    }
+        TextView username = findViewById(R.id.username);
+        TextView nombre = findViewById(R.id.nombreapellido);
+        username.setText(person.getResults().get(0).getLogin().getUsername());
+        nombre.setText(person.getResults().get(0).getName().getFirst() + " " +  person.getResults().get(0).getName().getLast());
 
-    public void abrirOtraActivity4(View view){
-        Intent intent = new Intent(this,CronometroActivity.class);
-        startActivity(intent);
+
+        ImageView imageView = findViewById(R.id.profile);
+        Picasso.get()
+                .load(person.getResults().get(0).getPicture().getLarge())
+                .resize(300, 300)
+                .error(R.drawable.logogoogle)
+                .into(imageView);
+
+        Button cronometro = findViewById(R.id.button3);
+        cronometro.setOnClickListener(view ->{
+            Intent crono = new Intent(Opciones.this, CronometroActivity.class);
+            startActivity(crono);
+        });
+        Button contador = findViewById(R.id.button4);
+        contador.setOnClickListener(view -> {
+            Intent conta = new Intent(Opciones.this, ContadorActivity.class);
+            startActivity(conta);
+        });
     }
 }
